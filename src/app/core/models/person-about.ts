58459dea6f7ba641a7f7dbf00b223e6393860da3
Marina Ruiz <marruiart@gmail.com>
2023-10-20 21:52:59 +0200

    Add personAbout model

---
+++ src/app/core/models/person-about.ts
@@ -0,0 +1,11 @@
+export interface PersonAbout {
+    id: number,
+    firstName: string,
+    lastName: string,
+    jobPosition: string,
+    image: string,
+    imageAlt: string,
+    githubUrl: string,
+    linkedinUrl: string,
+    description: string
+}
