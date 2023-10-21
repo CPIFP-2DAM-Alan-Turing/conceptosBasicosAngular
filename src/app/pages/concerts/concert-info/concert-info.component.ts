21f560480f8db1627ea99d3c3d9928f5f5bcbe50
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-10-21 11:03:20 +0200

    Add: concert-info component

---
+++ src/app/pages/concerts/concert-info/concert-info.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-concert-info',
+  templateUrl: './concert-info.component.html',
+  styleUrls: ['./concert-info.component.scss'],
+})
+export class ConcertInfoComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
