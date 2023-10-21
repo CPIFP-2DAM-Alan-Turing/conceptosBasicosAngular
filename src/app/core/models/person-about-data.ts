8517c2f4b30a2456ba00011b22097dbe959367de
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-10-21 09:50:07 +0200

    add: Información y foto de Juan Manuel Gutiérrez

--- src/app/core/models/person-about-data.ts
+++ src/app/core/models/person-about-data.ts
@@ -9,6 +9,16 @@ export const PERSONS_ABOUT = [
         githubUrl: "https://github.com/marruiart",
         linkedinUrl: "https://www.linkedin.com/in/marruiart/",
         description: "Me desenvuelvo bien en Java, HTML, CSS, JavaScript y SQL. Tengo conocimientos de PHP, Python y C. He utilizado tecnologias como Android, AWS, mongoDB, Node.js y Angular."
+    },
+    {
+        id: 2,
+        firstName: "Juan Manuel",
+        lastName: "Gutiérrez Mellado",
+        jobPosition: "Frontend Developer Jr.",
+        image: "assets/images/about/juan-manuel-gutierrez-avatar.png",
+        imageAlt: "Fotografía de Juan Manuel Gutiérrez",
+        githubUrl: "https://github.com/Juanma-Gutierrez",
+        linkedinUrl: "https://www.linkedin.com/in/juanmanuelgutierrezm/",
+        description: "Desarrollador de aplicaciones multiplataforma | Desarrollador de aplicaciones web. Estudios de HTML, CSS, JavasCript, Java, MySQL. Conocimientos de lenguajes C, Python, PHP, Kotlin y de las plataformas AWS y Docker"
     }
-    // TODO: añadir datos de Juanma
 ]
