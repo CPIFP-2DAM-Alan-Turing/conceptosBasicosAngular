06e85635a417602f133feccc723f116becbd0238
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-11-03 20:01:46 +0100

    Add home button in artists page

--- src/app/pages/artists/artists.page.ts
+++ src/app/pages/artists/artists.page.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
-  selector: 'app-artists',
-  templateUrl: './artists.page.html',
-  styleUrls: ['./artists.page.scss'],
+    selector: 'app-artists',
+    templateUrl: './artists.page.html',
+    styleUrls: ['./artists.page.scss'],
 })
 export class ArtistsPage implements OnInit {
+    loading = false;
 
-  constructor() { }
+    constructor(
+        private router: Router
+    ) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
+    home() {
+        this.router.navigate(['/home']);
+    }
 }
