a623ba3f6120b44e39368e5a1b51224098cdd6af
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-11-03 22:06:58 +0100

    Add artists cards list working

--- src/app/pages/artists/components/artist-info/artist-info.component.ts
+++ src/app/pages/artists/components/artist-info/artist-info.component.ts
@@ -1,14 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Artist } from 'src/app/core/models/artist.model';
 
 @Component({
-  selector: 'app-artist-info',
-  templateUrl: './artist-info.component.html',
-  styleUrls: ['./artist-info.component.scss'],
+    selector: 'app-artist-info',
+    templateUrl: './artist-info.component.html',
+    styleUrls: ['./artist-info.component.scss'],
 })
-export class ArtistInfoComponent  implements OnInit {
+export class ArtistInfoComponent implements OnInit {
+    @Input() artist?: Artist;
+    constructor() { }
 
-  constructor() { }
-
-  ngOnInit() {}
+    ngOnInit() { }
 
 }
