98280ca154755e0cc6316ececa3af478c1858a13
Juan Manuel Gutierrez <juanmanuelgutierrezmellado@gmail.com>
2023-11-04 16:25:28 +0100

    Add artist selectable component

---
+++ src/app/shared/components/artist-selectable/artist-selectable.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-artist-selectable',
+  templateUrl: './artist-selectable.component.html',
+  styleUrls: ['./artist-selectable.component.scss'],
+})
+export class ArtistSelectableComponent  implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,18 +3,21 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { IonicModule } from '@ionic/angular';
 import { ButtonEffectDirective } from './directives/button-effect.directive';
+import { ArtistSelectableComponent } from './components/artist-selectable/artist-selectable.component';
 
 @NgModule({
   declarations: [
     // Directives
     ButtonEffectDirective,
+    // Components
+    ArtistSelectableComponent
   ],
   imports: [
     // Modules
     CommonModule,
     ReactiveFormsModule,
     FormsModule,
-    IonicModule
+    IonicModule,
   ],
   exports: [
     // Modules
@@ -24,6 +27,8 @@ import { ButtonEffectDirective } from './directives/button-effect.directive';
     IonicModule,
     // Directives
     ButtonEffectDirective,
+    // Components
+    ArtistSelectableComponent
   ]
 })
 export class SharedModule { }
